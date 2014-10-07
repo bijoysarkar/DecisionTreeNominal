6fe428b0f4ccb258d3f6f22f53334f63b5f369f2
bijoysarkar <bijoy.kr.sarkar@gmail.com>
2014-10-07 02:43:30 -0500

    RootGainInfo

--- src/DecisionTreeImpl.java
+++ src/DecisionTreeImpl.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -59,6 +59,7 @@ public class DecisionTreeImpl extends DecisionTree {
 	public String classify(Instance instance) {
 
 		// TODO: add code here
+		return null;
 	}
 
 	@Override
@@ -102,10 +103,60 @@ public class DecisionTreeImpl extends DecisionTree {
 	@Override
 	public void rootInfoGain(DataSet train) {
 
-		
 		this.labels = train.labels;
 		this.attributes = train.attributes;
 		this.attributeValues = train.attributeValues;
-		// TODO: add code here
+
+		int instance_count = train.instances.size();
+		int label_counts[] = new int[labels.size()];
+
+		for (Instance instance : train.instances) {
+			label_counts[instance.label]++;
+		}
+
+		double totalEntropy = calculateEntropy(label_counts, instance_count);
+
+		for (int i = 0; i < attributes.size(); i++) {
+			// for the ith attribute
+			int number_of_attribute_values = attributeValues.get(
+					attributes.get(i)).size();
+			int attribute_counts[] = new int[number_of_attribute_values];
+			Map<Integer, int[]> map = new HashMap<>();
+			for (int j = 0; j < number_of_attribute_values; j++) {
+				map.put(j, new int[labels.size()]);
+			}
+
+			for (Instance instance : train.instances) {
+				int attributeValue = instance.attributes.get(i);
+				attribute_counts[attributeValue]++;
+				map.get(attributeValue)[instance.label]++;
+			}
+
+			double conditionalEntropy = 0;
+			for (int j = 0; j < number_of_attribute_values; j++) {
+				int attribute_count = attribute_counts[j];
+				double p = (double) attribute_count / instance_count;
+				conditionalEntropy = conditionalEntropy + p
+						* calculateEntropy(map.get(j), attribute_count);
+			}
+			System.out
+					.println(attributes.get(i)
+							+ " "
+							+ String.format("%.5f",
+									(totalEntropy - conditionalEntropy)));
+		}
+	}
+
+	private double calculateEntropy(int[] label_counts, int total_count) {
+		double result = 0;
+		for (int label_count : label_counts) {
+			double p = (double) label_count / total_count;
+			if (p != 0)
+				result = result - p * Math.log(p) / Math.log(2);
+		}
+		return result;
+	}
+	
+	
 	
 }
